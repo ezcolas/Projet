a345cb85bf01402991191733e5877f9dc3475caf
colasez <ezechielcolas@gmail.com>
2014-03-11 22:50:01 -0400

    Mon projet

---
+++ Gestion_cours/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from Gestion_cours.models import *
+
+
+admin.site.register(Professeur)
+admin.site.register(Etablissement)
+admin.site.register(cour)
+admin.site.register(cours_specialise)
+admin.site.register(description_cour)
